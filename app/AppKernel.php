f7658743aea47add8aadfdc39178413d7433ee13
Guillaume MOREL <guillaume.morel@verylastroom.fr>
2015-04-20 22:00:02 +0200

    Register KernelBundle and GmorelStateWorkflowBundle in SF2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle()
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Gmorel\StateWorkflowBundle\GmorelStateWorkflowBundle(),
+            new KernelBundle\KernelBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
